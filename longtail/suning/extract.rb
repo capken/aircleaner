30ec4b2d0c0336e541651541e5d93f00f6f136d0
Allen Zheng <capken@gmail.com>
2014-07-19 14:42:00 +0800

    improvements on suning extraction rule

--- longtail/suning/extract.rb
+++ longtail/suning/extract.rb
@@ -10,50 +10,21 @@ STDIN.each do |url|
   cached_file = File.open "./cache/#{hash}"
 
   doc = Nokogiri::HTML(cached_file.read)
-  doc.css("div#canshu_box").each do |table|
-    content = table.to_str
 
-    obj = {}
-    obj["brand"] = $1 if content =~ /品牌：\s*([^\n]+)/
-    obj["model"] = $1 if content =~ /型号：\s*([^\n]+)/
-    obj["made_in"] = $1 if content =~ /产地：\s*([^\n]+)/
-    obj["color"] = $1 if content =~ /颜色：\s*([^\n]+)/
-    obj["filter_lifetime"] = $1.to_f if content =~ /滤网使用寿命：\s*(\d+)月/
-    obj["filter_material"] = $1 if content =~ /过滤网材质：\s*([^\n]+)/
-    obj["timing"] = $1 if content =~ /定时功能：\s*([^\n]+)/
-    obj["remote_control"] = $1 if content =~ /遥控功能：\s*([^\n]+)/
-    obj["sleep_mode"] = $1 if content =~ /睡眠模式：\s*([^\n]+)/
-    obj["filter_replacement_reminder"] = $1 if content =~ /过滤网更新提醒：\s*([^\n]+)/
-    obj["air_quality_led"] = $1 if content =~ /空气质量显示：\s*([^\n]+)/
-
-    if content =~ /产品尺寸.+?(\d+)\*(\d+)\*(\d+)毫米/m
-      dimensions = {
-        "height" => $1.to_i,
-        "width"  => $2.to_i,
-        "deep"   => $3.to_i
-      }
+  input = {}
+  doc.css("div#canshu_box table tr").each do |tr|
+    tds = tr.css("td")
+    if tds.size == 3
+      label = tds[0].to_str.gsub(/：/, '').strip
+      input[label] = tds[1].to_str
     end
-    obj["dimensions"] = dimensions || {}
 
-    obj["weight"] = $1 if content =~ /净重：\s*([^\n]+)千克/
-
-    obj["CADR"] =  {}
-
-    obj["noise_level"] = {}
-
-    power = {
-      "max" => $1.to_i
-    } if content =~ /额定功率：\s*([^\n]+)瓦特/
-    obj["power"]= power || {}
-
-    obj["air_volume"] = $1.to_i if content =~ /风量：\s*(\d+)立方\/时/
-    obj["total_fan_speed_levels"] = -1
-    obj["_source"] = url.strip
+    input["_source"] = url.strip
 
     doc.css("div#PicView img").each do |image|
-      obj["image"] = URI.join url.strip, image["src"]
+      input["image"] = URI.join url.strip, image["src"]
     end
-
-    puts obj.to_json
   end
+
+  puts input.to_json
 end
