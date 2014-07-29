cde543d29897916e6e805fba89e75f4f58bd6deb
Allen Zheng <capken@gmail.com>
2014-07-29 14:09:44 +0800

    add the query and suggest api

--- web/app.rb
+++ web/app.rb
@@ -17,3 +17,26 @@ get '/products/:id' do
   product = Product.find(params[:id]) || {}
   json product
 end
+
+get '/products' do
+  condition = {}
+  ['brand', 'model'].each do |attr|
+    condition[attr] = params[attr] if params[attr]
+  end
+
+  products = Product.where(condition)
+
+  json 'products' => products
+end
+
+get '/suggest' do
+
+  room_size = params['room_size'].to_f
+  min_cadr = room_size * 2.8 * 5
+
+  condition = "cadr_dust >= #{min_cadr}"
+  products = Product.where(condition).
+    order('cadr_dust').limit(10)
+
+  json 'products' => products
+end
