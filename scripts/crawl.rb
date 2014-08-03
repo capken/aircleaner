ae0de3cce6006029e0e4c4fdd57ed89a107f94ba
Allen Zheng <capken@gmail.com>
2014-08-03 22:17:03 +0800

    improvements on crawling script

--- scripts/crawl.rb
+++ scripts/crawl.rb
@@ -2,12 +2,16 @@ require 'digest/sha1'
 
 system "mkdir ./cache" unless Dir.exists? "./cache"
 
-encode = ARGV[0] || "utf-8"
+encode = ARGV[0]
 
 STDIN.each do |url|
   url = url.strip
   hash = Digest::SHA1.hexdigest(url)
   warn "caching #{url} into ./cache/#{hash} ..."
 
-  system "curl -L #{url} | iconv -f #{encode} -t utf-8 > ./cache/#{hash}"
+  if encode
+    system "curl -L #{url} | iconv -f #{encode} -t utf-8 > ./cache/#{hash}"
+  else
+    system "curl -L #{url} > ./cache/#{hash}"
+  end
 end
